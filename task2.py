e20c93d1712e8314e30d3e1b2bec56ca613689cb
rush1007 <80176837+rush1007@users.noreply.github.com>
2021-03-10 09:51:41 -0700

    Update task2.py

--- task2.py
+++ task2.py
@@ -120,7 +120,7 @@ for x in range(2000, 3200):
 # 7. Write a program that prints all the numbers from 0 to 6 except 3 and 6.Expected output: 0 1 2 4 5
 # Note: Use ‘continue’ statement
 
-for x in range(0, 6):
+for x in range(7):
     if x == 3 or x == 6:
         continue
     print(x)
